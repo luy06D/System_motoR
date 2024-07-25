9863861dd8d7cba724962c1197663d54ba703098
luy06D <cusiluis04@gmail.com>
2024-07-24 23:41:46 -0500

    new cadena connection

--- src/Connection/Conexion.java
+++ src/Connection/Conexion.java
@@ -14,33 +14,24 @@ public class Conexion {
         Connection conn;
 
     public Connection getConexion() {
-        try {
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=PRUEBA", "root", "password");
-        } catch (ClassNotFoundException | SQLException ex) {
-            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return conn;
-    }
-    
-      public static void main(String[] args) {
-        Conexion conexion = new Conexion();
-        Connection conn = conexion.getConexion();
         
-        if (conn != null) {
-            System.out.println("La conexión se ha establecido correctamente.");
-            try {
-                Statement stmt = conn.createStatement();
-                ResultSet rs = stmt.executeQuery("SELECT 1");
-                if (rs.next()) {
-                    System.out.println("La consulta de prueba fue exitosa: " + rs.getInt(1));
-                }
-            } catch (SQLException ex) {
-                System.out.println("Error al ejecutar la consulta: " + ex.getMessage());
-            }
-        } else {
-            System.out.println("No se pudo establecer la conexión.");
+        String cadenaConecction = "jdbc:sqlserver://localhost:1433;"
+                + "database=PRUEBA;"
+                + "user=luisd;"
+                + "password =060903;"
+                + "loginTimeout=30;";
+        try{
+            Connection con = DriverManager.getConnection(cadenaConecction);
+            return con;
+            
+        }catch(SQLException ex){
+            System.out.println(ex.toString());
+            return null;
+            
         }
+
+    
     }
+
     
 }
